b8abede566fad1b337d23fb14d7223e9f655ec2a
Syntax#2793 <35030892+nesique@users.noreply.github.com>
2023-04-08 21:26:58 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bot = require('./src/index.js');
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+const listener = app.listen(process.env.PORT, () => {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
